44aedf1e670d2fa91d0a7f1d2e2860177974c0d2
Dominik Sellin <doms@netcompany.com>
2022-05-19 13:27:59 +0200

    Added API controller

---
+++ OceanicAirlinesWebApp/Controllers/RouteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OceanicAirlinesWebApp.Models;
+
+namespace OceanicAirlinesWebApp.Controllers
+{
+    [Route("api/route")]
+    [ApiController]
+    public class RouteController : ControllerBase { 
+
+        public ResultDTO GetRoute(RequestDTO request)
+        {
+            return new ResultDTO()
+            {
+                Price = 0,
+                Time = 0
+            };
+        }
+
+    }
+}
