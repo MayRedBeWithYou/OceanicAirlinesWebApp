c1badc89e77462147d1fe6af7dccf1ff9f552b11
Mathias Hagerup Dolmer <mhd@netcompany.com>
2022-05-20 00:44:03 +0200

    added automatic invoice email

---
+++ OceanicAirlinesWebApp/Algorithms/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+
+
+namespace OceanicAirlinesWebApp.Algorithms
+{
+    public class Invoice
+    {
+            public void sendEmail(string transactionId, string customerName, string customerPrice, string customerFromLocation, string customerToLocation)
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new System.Net.NetworkCredential("cesoairlines@gmail.com", "oceanicFlyAway16"),
+                    EnableSsl = true,
+                };
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("cesoairlines@gmail.com"),
+                    Subject = "OA INVOICE #" + transactionId,
+                    Body = "<p> Hello " + customerName + "!" + "<br><br> Invoice for your parcel delivery going from " + customerFromLocation + " to " + customerToLocation + ". <br> The total price for the parcel delivery service was $" + customerPrice + ". <br><br> Thanks for using Oceanic Airlines as you preferred parcel delivery service in Africa!",
+                    IsBodyHtml = true,
+                };
+
+                mailMessage.To.Add("cesoairlines@gmail.com");
+                smtpClient.Send(mailMessage);
+
+            }
+    }
+}
