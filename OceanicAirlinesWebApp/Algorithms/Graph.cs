dd0d2991c7f164dd231a0ebafff8b34469f173c1
Dominik Sellin <doms@netcompany.com>
2022-05-19 09:21:27 +0200

    Set up Web App

--- OceanicAirlinesWebApp/Algorithms/Graph.cs
+++ OceanicAirlinesWebApp/Algorithms/Graph.cs
@@ -1,48 +1,51 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public enum EdgeType
+namespace OceanicAirlinesWebApp.Algorithms
 {
-    Airline,
-    Car,
-    Ship
-}
-
-public class Edge
-{
-    public int From { get; set; }
-    public int To { get; set; }
-    public EdgeType Type { get; set; }
-    public int Price { get; set; }
-
-}
-
-public class Node
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public List<Edge> Edges { get; set; }
-}
-
-public class Route
-{
-    public List<Node> route { get; set; }
-
-    public int TotalPrice { get; set; }
-
-    public int TotalTime { get; set; }
-}
-
-public class Graph
-{
-    public List<Edge> Edges = new List<Edge>();
-    public List<Node> Nodes = new List<Node>();
-
-    public Route Dijkstra(Func<Edge, int> edgeWeight)
+    public enum EdgeType
     {
-        return null;
+        Airline,
+        Car,
+        Ship
+    }
+
+    public class Edge
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public EdgeType Type { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public class Node
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Edge> Edges { get; set; }
+    }
+
+    public class Route
+    {
+        public List<Node> route { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public int TotalTime { get; set; }
+    }
+
+    public class Graph
+    {
+        public List<Edge> Edges = new List<Edge>();
+        public List<Node> Nodes = new List<Node>();
+
+        public Route Dijkstra(Func<Edge, int> edgeWeight)
+        {
+            return null;
+        }
     }
 }
--- OceanicAirlinesWebApp/Models/ErrorViewModel.cs
+++ OceanicAirlinesWebApp/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace OceanicAirlinesWebApp.Models;
-
-public class ErrorViewModel
+namespace OceanicAirlinesWebApp.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
