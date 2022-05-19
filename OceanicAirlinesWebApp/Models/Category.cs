f9119c6d7ba9177c30ca2e49763bb98f259ba5b7
Mathias Hagerup Dolmer <mhd@netcompany.com>
2022-05-19 13:12:19 +0200

    added new entities

---
+++ OceanicAirlinesWebApp/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace OceanicAirlinesWebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double AddedPrice { get; set; }
+    }
+}
