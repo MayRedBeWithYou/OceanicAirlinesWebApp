7a29993b9fda9c8be293756c6a59ccddfb8acce9
Dominik Sellin <doms@netcompany.com>
2022-05-19 10:57:53 +0200

    Added DBContext to the solution

---
+++ OceanicAirlinesWebApp/Data/OceanicAirlinesWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OceanicAirlinesWebApp.Models;
+
+namespace OceanicAirlinesWebApp.Data
+{
+    public class OceanicAirlinesWebAppContext : DbContext
+    {
+        public OceanicAirlinesWebAppContext (DbContextOptions<OceanicAirlinesWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OceanicAirlinesWebApp.Models.Parcel>? Parcel { get; set; }
+    }
+}
--- OceanicAirlinesWebApp/Program.cs
+++ OceanicAirlinesWebApp/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OceanicAirlinesWebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<OceanicAirlinesWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OceanicDBContext") ?? throw new InvalidOperationException("Connection string 'OceanicAirlinesWebAppContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
