60d49d7e0512a5662fb1ef99704cefaed0b8c9d3
Dominik Sellin <doms@netcompany.com>
2022-05-19 21:59:54 +0200

    Added unit tests project (NUnit)

---
+++ UnitTest/AlgorithmTests.cs
@@ -0,0 +1,28 @@
+namespace UnitTest
+{
+    public class AlgorithmTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Dijkstra_returns_airline_route()
+        {
+            Assert.AreEqual(0, 4);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UnitTest/ApiTests.cs
@@ -0,0 +1,24 @@
+﻿using OceanicAirlinesWebApp.Algorithms;
+using OceanicAirlinesWebApp.Models;
+namespace UnitTest
+{
+    public class ApiTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            var result = Endpoints.FetchResultFromTL(new RequestDTO()
+            {
+                From = "Warsaw",
+                To = "Paris"
+            });
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
